021adad1f59d6f453701e9556fe7e2bccf89313d
smalleha <1561155732@qq.com>
2025-06-13 16:19:29 +0800

    add agx mycobot 280

---
+++ mycobot_ros2/mecharm/mecharm_pi/setup.py
@@ -0,0 +1,63 @@
+import os
+from setuptools import setup, __version__ as setuptools_version
+from packaging.version import Version
+from glob import glob
+
+package_name = 'mecharm_pi'
+
+# 检查 setuptools 版本
+use_dash_separated_options = Version(setuptools_version) < Version("58.0.0")
+
+
+# 动态生成 setup.cfg 内容
+setup_cfg_content = """
+[develop]
+{script_option}=$base/lib/{package_name}
+
+[install]
+{install_scripts_option}=$base/lib/{package_name}
+""".format(
+    package_name=package_name,
+    script_option='script-dir' if use_dash_separated_options else 'script_dir',
+    install_scripts_option='install-scripts' if use_dash_separated_options else 'install_scripts'
+)
+
+# 将内容写入 setup.cfg
+with open("setup.cfg", "w") as f:
+    f.write(setup_cfg_content)
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+         # launch 文件路径
+        (os.path.join('share', package_name, "launch"), glob('launch/*.launch.py')),
+        # python 文件
+        # (os.path.join('lib',package_name),glob(package_name+'/*.py')),
+        # 配置文件
+        (os.path.join('share', package_name, "config"), glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='u20',
+    maintainer_email='u20@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'detect_marker = mecharm_pi.detect_marker:main',
+            'follow_display = mecharm_pi.follow_display:main',
+            'following_marker = mecharm_pi.following_marker:main',
+            'listen_real_of_topic = mecharm_pi.listen_real_of_topic:main',
+            'listen_real = mecharm_pi.listen_real:main',
+            'simple_gui = mecharm_pi.simple_gui:main',
+            'slider_control = mecharm_pi.slider_control:main',
+            'teleop_keyboard = mecharm_pi.teleop_keyboard:main',
+        ],
+    },
+)
---
+++ mycobot_ros2/mycobot_280/mycobot_280/mycobot_280/teleop_keyboard.py
@@ -0,0 +1,190 @@
+from __future__ import print_function
+import sys
+import termios
+import tty
+import time
+import os
+import pymycobot
+from packaging import version
+
+# min low version require
+MIN_REQUIRE_VERSION = '3.6.1'
+
+current_verison = pymycobot.__version__
+print('current pymycobot library version: {}'.format(current_verison))
+if version.parse(current_verison) < version.parse(MIN_REQUIRE_VERSION):
+    raise RuntimeError('The version of pymycobot library must be greater than {} or higher. The current version is {}. Please upgrade the library version.'.format(MIN_REQUIRE_VERSION, current_verison))
+else:
+    print('pymycobot library version meets the requirements!')
+    from pymycobot import MyCobot280
+
+msg = """\
+Mycobot Teleop Keyboard Controller
+---------------------------
+Movimg options(control coordinations [x,y,z,rx,ry,rz]):
+              w(x+)
+
+    a(y-)     s(x-)     d(y+)
+
+    z(z-) x(z+)
+
+u(rx+)   i(ry+)   o(rz+)
+j(rx-)   k(ry-)   l(rz-)
+
+Gripper control:
+    g - open
+    h - close
+
+Pump control:
+    b - open
+    m - close
+
+Other:
+    1 - Go to init pose
+    2 - Go to home pose
+    3 - Resave home pose
+    q - Quit
+"""
+
+
+def vels(speed, turn):
+    return "currently:\tspeed: %s\tchange percent: %s  " % (speed, turn)
+
+
+class Raw(object):
+    def __init__(self, stream):
+        self.stream = stream
+        self.fd = self.stream.fileno()
+
+    def __enter__(self):
+        self.original_stty = termios.tcgetattr(self.stream)
+        tty.setcbreak(self.stream)
+
+    def __exit__(self, type, value, traceback):
+        termios.tcsetattr(self.stream, termios.TCSANOW, self.original_stty)
+
+
+def teleop_keyboard():
+    # 优先检查 ttyACM 端口
+    robot_acm = os.popen("ls /dev/ttyACM* 2>/dev/null").readline().strip()
+    # 然后检查 ttyUSB 端口
+    robot_usb = os.popen("ls /dev/ttyUSB* 2>/dev/null").readline().strip()
+
+    if robot_acm:       # 优先使用 ttyACM 设备
+        port = robot_acm
+        print("使用 ttyACM 端口:", robot_acm)
+    elif robot_usb:     # 如果 ttyACM 不存在，则使用 ttyUSB
+        port = robot_usb
+        print("使用 ttyUSB 端口:", robot_usb)
+    else:
+        raise Exception("未检测到任何串口设备")
+
+    print("port:%s, baud:%d" % (port, 115200))
+    mc = MyCobot280(port, 115200)
+    time.sleep(0.05)
+    mc.set_fresh_mode(1)
+    time.sleep(0.05)
+
+    model = 0
+    speed = 30
+    change_percent = 5
+
+    change_angle = 180 * change_percent / 100
+    change_len = 250 * change_percent / 100
+
+    init_pose = [[0, 0, 0, 0, 0, 0], speed]
+    home_pose = [[0, 8, -127, 40, 0, 0], speed]
+
+    mc.send_angles(*init_pose)
+
+    while True:
+        res = mc.get_coords()
+        if res:
+            break
+        time.sleep(0.1)
+
+    record_coords = [res, speed, model]
+
+    try:
+        print(msg)
+        print(vels(speed, change_percent))
+        while 1:
+            try:
+                print("\r current coords: %s" % record_coords)
+                with Raw(sys.stdin):
+                    key = sys.stdin.read(1)
+                if key == "q":
+                    mc.release_all_servos()
+                    break
+                elif key in ["w", "W"]:
+                    record_coords[0][0] += change_len
+                    mc.send_coords(*record_coords)
+                elif key in ["s", "S"]:
+                    record_coords[0][0] -= change_len
+                    mc.send_coords(*record_coords)
+                elif key in ["a", "A"]:
+                    record_coords[0][1] -= change_len
+                    mc.send_coords(*record_coords)
+                elif key in ["d", "D"]:
+                    record_coords[0][1] += change_len
+                    mc.send_coords(*record_coords)
+                elif key in ["z", "Z"]:
+                    record_coords[0][2] -= change_len
+                    mc.send_coords(*record_coords)
+                elif key in ["x", "X"]:
+                    record_coords[0][2] += change_len
+                    mc.send_coords(*record_coords)
+                elif key in ["u", "U"]:
+                    record_coords[0][3] += change_angle
+                    mc.send_coords(*record_coords)
+                elif key in ["j", "J"]:
+                    record_coords[0][3] -= change_angle
+                    mc.send_coords(*record_coords)
+                elif key in ["i", "I"]:
+                    record_coords[0][4] += change_angle
+                    mc.send_coords(*record_coords)
+                elif key in ["k", "K"]:
+                    record_coords[0][4] -= change_angle
+                    mc.send_coords(*record_coords)
+                elif key in ["o", "O"]:
+                    record_coords[0][5] += change_angle
+                    mc.send_coords(*record_coords)
+                elif key in ["l", "L"]:
+                    record_coords[0][5] -= change_angle
+                    mc.send_coords(*record_coords)
+                elif key in ["g", "G"]:
+                    mc.set_gripper_state(0, 80)
+                elif key in ["h", "H"]:
+                    mc.set_gripper_state(1, 80)
+                elif key in ["b", "B"]:
+                    mc.set_basic_output(2, 0)
+                    mc.set_basic_output(5, 0)
+                elif key in ["m", "M"]:
+                    mc.set_basic_output(2, 1)
+                    mc.set_basic_output(5, 1)
+                elif key == "1":
+                    mc.send_angles(*init_pose)
+                elif key in "2":
+                    mc.send_angles(*home_pose)
+                elif key in "3":
+                    rep = mc.get_angles()
+                    home_pose[0] =rep
+                else:
+                    continue
+
+            except Exception as e:
+                # print(e)
+                continue
+
+            time.sleep(1)
+
+    except Exception as e:
+        print(e)
+
+
+def main():
+    teleop_keyboard()
+
+
+if __name__ == "__main__":
+    main()
